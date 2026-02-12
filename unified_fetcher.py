ea1aed2d52d7c6dffb6b1db33b2cfc93c49e3c5f
shengjianqi <133628837+shengjianqi@users.noreply.github.com>
2026-02-12 18:05:08 +0800

    unified_fetcher.py

--- unified_fetcher.py
+++ unified_fetcher.py
@@ -1,30 +1,12 @@
-
 from arxiv_fetcher import ArxivFetcher
-from prl_fetcher import PRLFetcher
 from config import Config
-
 
 class UnifiedPaperFetcher:
     def __init__(self):
         self.arxiv = ArxivFetcher()
-        self.prl = PRLFetcher(Config.SEARCH_KEYWORDS)
+        self.prl = None   # PRL 模块暂时禁用
 
-    def fetch_all(self, days_back=1):
-        arxiv_papers = self.arxiv.fetch_recent_papers(days_back)
-        prl_papers = self.prl.fetch_recent_papers(days_back)
-
-        all_papers = arxiv_papers + prl_papers
-
-        # 基于 title 去重
-        seen = set()
-        unique = []
-        for p in all_papers:
-            t = p['title'].lower().strip()
-            if t not in seen:
-                seen.add(t)
-                unique.append(p)
-
-        return unique
-
-    def generate_summary(self, paper):
-        return self.arxiv.generate_summary(paper)
+    def fetch_all(self):
+        papers = []
+        papers.extend(self.arxiv.fetch())
+        return papers
